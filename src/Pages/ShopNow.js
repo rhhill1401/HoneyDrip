530797983f672e0f8b40d5d6dc7fa219fec625a3
Rudolph Terry Hill <terryhill@Rudolphs-iMac.local>
2021-07-13 16:00:21 -0500

    main product update

--- src/Pages/ShopNow.js
+++ src/Pages/ShopNow.js
@@ -16,7 +16,6 @@ class ShopNow extends React.Component {
     };
   }
 
-products-components
   render() {
     return (
       <div>
@@ -66,43 +65,6 @@ products-components
           </main>
           <footer>All right is reserved.</footer>
         </div>
-
-export const ShopNow = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <div className="grid-container">
-        <header>
-          <Ul>
-            <Li>
-              {" "}
-              <Lia href="/">React Shopping Cart</Lia>{" "}
-            </Li>
-            <Li>
-              {" "}
-              <Lia href="/preRolls">Pre-Rolls</Lia>{" "}
-            </Li>
-            <Li>
-              {" "}
-              <Lia href="/">Vaporizers</Lia>{" "}
-            </Li>
-            <Li>
-              {" "}
-              <Lia href="/">Edibiles</Lia>
-            </Li>
-            <Li>
-              {" "}
-              <Lia href="/">Tropicals</Lia>
-            </Li>
-            <Li>
-              {" "}
-              <Lia href="/">Clothing</Lia>
-            </Li>
-          </Ul>
-        </header>
-        <main>Product List</main>
-        <footer>All right is reserved.</footer>
-
       </div>
     );
   }
